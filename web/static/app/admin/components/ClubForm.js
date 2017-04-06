04834dc0fc72e51bc87d42eb154daa963f1fed96
pashtetkun <pavel.c@sunrise-r.ru>
2017-04-06 18:49:14 +0300

    заготовка формы клуба

--- web/static/app/admin/components/ClubForm.js
+++ web/static/app/admin/components/ClubForm.js
@@ -15,27 +15,83 @@ class ClubForm extends React.Component {
 		const { handleSubmit } = this.props
 
 		var layout = [
-      			{i: 'vsol_name_label',    x: 0, y: 0, w: 1, h: 1, static: true},
-      			{i: 'vsol_name', 	      x: 1, y: 0, w: 1, h: 1, static: true},
-      			{i: 'vsol_stadium_label', x: 0, y: 1, w: 1, h: 1, static: true},
-      			{i: 'vsol_stadium', 	  x: 1, y: 1, w: 1, h: 1, static: true},
-      			{i: 'submit_btn', 	      x: 6, y: 3, w: 3, h: 1, static: true}
+				{i: 'vsol_logo',    	  		   x: 1, y: 0, w: 5, h: 3, static: true},
+				{i: 'btn_copy_logo',     		   x: 6, y: 1, w: 1, h: 1, static: true},
+				{i: 'logo',    	  		  		   x: 7, y: 0, w: 5, h: 3, static: true},
+      			{i: 'vsol_name_label',    		   x: 0, y: 3, w: 1, h: 1, static: true},
+      			{i: 'vsol_name', 	      		   x: 1, y: 3, w: 5, h: 1, static: true},
+      			{i: 'btn_copy_name_and_city',      x: 6, y: 3, w: 1, h: 1, static: true},
+      			{i: 'name', 	      		   	   x: 7, y: 3, w: 4, h: 1, static: true},
+      			{i: 'city', 	      		   	   x: 9, y: 3, w: 4, h: 1, static: true},
+      			{i: 'vsol_stadium_label', 		   x: 0, y: 4, w: 1, h: 1, static: true},
+      			{i: 'vsol_stadium', 	  		   x: 1, y: 4, w: 5, h: 1, static: true},
+      			{i: 'btn_copy_stadium',     	   x: 6, y: 4, w: 1, h: 1, static: true},
+      			{i: 'stadium', 	  		   		   x: 7, y: 4, w: 5, h: 1, static: true},
+      			{i: 'transfermarkt_label', 	  	   x: 0, y: 6, w: 1, h: 1, static: true},
+      			{i: 'transfermarkt', 	  	   	   x: 1, y: 6, w: 5, h: 1, static: true},
+      			{i: 'status_label', 	  	   	   x: 5, y: 6, w: 1, h: 1, static: true},
+      			{i: 'status', 	  	   	   		   x: 6, y: 6, w: 4, h: 1, static: true},
+      			{i: 'notes_label', 	  		   	   x: 0, y: 7, w: 1, h: 1, static: true},
+      			{i: 'notes', 	  		   		   x: 1, y: 7, w: 5, h: 3, static: true},
+      			{i: 'submit_btn', 	      		   x: 6, y: 11, w: 4, h: 1, static: true}
     	];
 		
 		return (
 				<form onSubmit={handleSubmit}>
-					<ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+					<ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1024}>
+						<div key={'vsol_logo'}>
+							<Field name="vsol_logo_url" component="input" type="text"/>
+						</div>
+						<div key={'btn_copy_logo'}>
+							<button type="button">--></button>
+						</div>
+						<div key={'logo'}>
+							<Field name="logo_url" component="input" type="text"/>
+						</div>
         				<div key={'vsol_name_label'}>
           					<label htmlFor="vsol_name">Название</label>
         				</div>
         				<div key={'vsol_name'}>
-        					<Field name="vsol_name" component="input" type="text"/>
+        					<Field name="vsol_name" component="input" type="text" disabled={true}/>
+        				</div>
+        				<div key={'btn_copy_name_and_city'}>
+							<button type="button">--></button>
+						</div>
+						<div key={'name'}>
+        					<Field name="name" component="input" type="text"/>
+        				</div>
+        				<div key={'city'}>
+        					<Field name="city" component="input" type="text"/>
         				</div>
         				<div key={'vsol_stadium_label'}>
           					<label htmlFor="vsol_stadium">Стадион</label>
         				</div>
         				<div key={'vsol_stadium'}>
-        					<Field name="vsol_stadium" component="input" type="text"/>
+        					<Field name="vsol_stadium" component="input" type="text" disabled={true}/>
+        				</div>
+        				<div key={'btn_copy_stadium'}>
+							<button type="button">--></button>
+						</div>
+						<div key={'stadium'}>
+        					<Field name="stadium" component="input" type="text"/>
+        				</div>
+        				<div key={'transfermarkt_label'}>
+          					<label htmlFor="transfermarkt_url">Трансфермаркт</label>
+        				</div>
+        				<div key={'transfermarkt'}>
+        					<Field name="transfermarkt_url" component="input" type="text"/>
+        				</div>
+        				<div key={'notes_label'}>
+          					<label htmlFor="notes">Заметки</label>
+        				</div>
+        				<div key={'notes'}>
+        					<Field name="notes" component="input" type="textarea"/>
+        				</div>
+        				<div key={'status'}>
+          					<label htmlFor="status">Статус</label>
+        				</div>
+        				<div key={'status'}>
+        					<Field name="status" component="input" type="text"/>
         				</div>
         				<div key={'submit_btn'}>
         					<button type="submit">Submit</button>
